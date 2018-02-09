9957d895eadbaeb7c2baadc5f7f09acb0fc8d775
Paul-Yves <lucas.paulyves@gmail.com>
2018-02-09 10:41:23 +0100

    controllers return 404 if resource does not exist

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -3,6 +3,7 @@ class CompanyController < ApplicationController
   # display all the candidates for a specific company
   def show
     company = Company.find_by(name: 'Night\'s Watch') # this is stub for demo, should use company id instead
+    return render nothing: true, status: 404 unless company
     render json: company
   end
 end
--- app/controllers/recruitment_controller.rb
+++ app/controllers/recruitment_controller.rb
@@ -2,6 +2,7 @@ class RecruitmentController < ApplicationController
   def update
     recruitment = Recruitment.find(params[:recruitment_id])
     step = params.require(:step)
+    return render nothing: true, status: 404 unless recruitment
     recruitment.update(step: step)
     ActionCable.server.broadcast('dashboard_channel', id: recruitment.id, message: 'step_changed')
   end
